ace4348ee343d93e2f2124e80b2c848ab1d01536
Dalmasio <dalmasiof@gmail.com>
2022-05-22 16:14:43 -0300

    fcria login ao criar cliente

--- B-BLL/Service/ClienteService.cs
+++ B-BLL/Service/ClienteService.cs
@@ -10,15 +10,27 @@ namespace CoverAll_API.B_BLL.Service
     public class ClienteService : IClienteService
     {
         private readonly IClienteRepository repository;
-        public ClienteService(IClienteRepository repository)
+        private readonly ILoginRepository repositoryLogin;
+
+        public ClienteService(IClienteRepository repository,ILoginRepository repositoryLogin)
         {
             this.repository = repository;
+            this.repositoryLogin = repositoryLogin;
 
         }
 
         public void Add(Cliente entity)
         {
             repository.Add(entity);
+            repository.SaveChanges();
+            var objLogin = new Login(){
+                Id=0,
+                IdCLiente=entity.Id,
+                Senha="admin",
+                Usuario=entity.email
+            };
+            repositoryLogin.Add(objLogin);
+
         }
 
         public void Delete(Cliente entity)
